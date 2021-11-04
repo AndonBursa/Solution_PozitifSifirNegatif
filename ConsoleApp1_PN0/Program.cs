3c8d8b896abe003177e93069d64c3e7d9ce12bbb
cosond <cosond@IABTR00239>
2021-11-04 12:50:31 +0300

    Add project files.

---
+++ ConsoleApp1_PN0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1_PN0
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int Sayi = 0;
+            string temp;
+
+            Console.WriteLine("Bir Sayı Giriniz");
+            Sayi = int.Parse(Console.ReadLine());
+
+            if(Sayi>0)
+            {
+                Console.WriteLine("Sayı Pozitif");
+            }
+            else if(Sayi<0)
+            {
+                Console.WriteLine("Sayı Negatif");
+            }
+
+            else
+            {
+                Console.WriteLine("Sayı Sıfır '0' dır");
+            }
+            Console.WriteLine("Çıkmak için bir tuşa basınız"); 
+            Console.ReadKey();
+        }
+    }
+}
